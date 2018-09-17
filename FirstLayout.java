e2ff0fd803ed4a07c25485e61cfc011f9c6f17f6
pjm001 <powerpjm603@gmail.com>
2018-09-17 21:19:28 +0900

    Thinkar

---
+++ FirstLayout.java
@@ -0,0 +1,59 @@
+package com.example.root.layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FirstLayout extends AppCompatActivity {
+
+    public static String memoData = "initial"; // 어플 종료 후에도 memoData 를 불러오기 위함
+
+    EditText mMemoEdit = null;
+    TextFileManager mTextFileManager = new TextFileManager(this);
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.first_layout);
+
+        mMemoEdit = (EditText) findViewById(R.id.memo_edit);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // 1. 파일에 저장된 메모 텍스트 파일 불러오기
+            case R.id.load_btn: {
+                memoData = mTextFileManager.load();
+                mMemoEdit.setText(memoData);
+
+                Toast.makeText(this, "불러오기 완료", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            // 2. 에디트텍스트에 입력된 메모를 텍스트 파일로 저장하기
+            case R.id.save_btn: {
+                memoData = mMemoEdit.getText().toString();
+                mTextFileManager.save(memoData);
+                mMemoEdit.setText("");
+
+                Toast.makeText(this, "저장 완료", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            // 3. 저장된 메모 파일 삭제하기
+            case R.id.delete_btn: {
+                mTextFileManager.delete();
+                mMemoEdit.setText("");
+
+                Toast.makeText(this, "삭제 완료", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
+
